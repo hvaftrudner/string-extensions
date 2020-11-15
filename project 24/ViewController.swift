99ee0f7dfabcb1febdfcec830c8809ae6ece35e2
hvaftrudner <krill_eriksson@hotmail.com>
2020-11-15 18:56:31 +0100

    Initial Commit

---
+++ project 24/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  project 24
+//
+//  Created by Kristoffer Eriksson on 2020-11-15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
